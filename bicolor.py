868f13b447dd2b797929c0627a284d4ca1765d58
^^ Bianca ^^ <bianca_uwu@yahoo.com>
2020-12-30 04:38:04 +0100

    Create bicolor.py

---
+++ bicolor.py
@@ -0,0 +1,26 @@
+import os
+import sys
+
+# You can basically do `import bicolor` and then `print(f"{bicolor.red("This is red!")} | This is normal!")`
+
+def color(text, color_code):
+    if sys.platform == "win32" and os.getenv("TERM") != "xterm":
+        return text
+
+    return '\x1b[%dm%s\x1b[0m' % (color_code, text)
+
+
+def red(text):
+    return color(text, 31)
+
+
+def blink(text):
+    return color(text, 5)
+
+
+def green(text):
+    return color(text, 32)
+
+
+def blue(text):
+    return color(text, 34)
